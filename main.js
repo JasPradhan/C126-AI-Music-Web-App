97f3dc1c5ffc0476a775caad8c2a239ac4257442
JasPradhan <70465360+JasPradhan@users.noreply.github.com>
2020-10-15 17:16:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ leftSong="";
 
 leftScore=0;
 
+rightSong="";
+
+rightScore=0;
+
 song_1="";
 song_2="";
 
@@ -28,20 +32,32 @@ poseNet.on('pose',gotPoses);
 function draw(){
 image(video, 0, 0, 600, 500);
 
-song_1.isPlaying()
+leftSong=song_1.isPlaying();
+
+rightSong=song_2.isPlaying();
 
 fill("#3cff00");
 strokeWeight(2);
 stroke("#ff0000");
 
-if(score_leftWrist>0.2){
-        circle(leftWristX ,leftWristY ,20);
-        song_2.stop()
-        if(song_1==false){
-            song_1.play()
-            document.getElementById("name_of_song").innerHTML="Song: Harry Potter Theme Song"
-        }
+if(leftScore>0.2){
+    circle(leftWristX ,leftWristY ,20);
+    song_2.stop()
+    if(leftSong==false){
+        song_1.play()
+        document.getElementById("name_of_song").innerHTML="Song: Harry Potter Theme Song";
+    }   
+}
+
+else if(rightScore>0.2){
+    circle(rightWristX ,rightWristY ,20);
+    song_1.stop()
+    if(rightSong==false){
+        song_2.play()
+        document.getElementById("name_of_song").innerHTML="Song: James Bond Theme Song";
     }
+}   
+
 }
 
 
@@ -57,7 +73,15 @@ function gotPoses(results){
 
         leftScore=results[0].pose.keypoints[9].score;
 
+        rightScore=results[0].pose.keypoints[10].score;
+
         rightWristX=results[0].pose.rightWrist.x;
         rightWristY=results[0].pose.rightWrist.y;
     }
+}
+
+function play(){
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
 }
